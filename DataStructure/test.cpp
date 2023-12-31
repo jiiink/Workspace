8e57daa361d163fba47d8722b76536493b986333
ByeongHyeon Kim <bhkim0404@gmail.com>
2024-01-01 03:49:02 +0900

    .

---
+++ DataStructure/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Enter the number\n";
+    int num;
+    cin >> num;
+    cout << "You entered " << num << endl;
+    return 0;
+}
