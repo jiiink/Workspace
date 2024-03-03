9abebb71aa712304716c75b477aaf2551cdda143
jiiink <bhkim0404@gmail.com>
2024-03-03 15:46:10 +0900

    .

---
+++ HelloWorld-App/Hello.rs
@@ -0,0 +1,8 @@
+fn
+main() {
+    println!("Rust says Hello to TutorialsPoint !!");
+    println!();
+    println!("format {} arguments", 100); // prints format some arguments
+    print!("hello")
+}
+
